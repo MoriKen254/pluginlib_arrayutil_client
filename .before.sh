cc8a6b84daf3ae55d62da3c330a28467500fe45e
MoriKen254 <masamorobo@gmail.com>
2017-06-04 01:32:47 +0900

    Check travis

--- .before.sh
+++ .before.sh
@@ -1,6 +1,6 @@
--#!/bin/bash		
- -		
- -pwd		
+#!/bin/bash		
+		
+pwd		
 ls		
 cd ../../catkin_ws/src		
 pwd		
@@ -8,7 +8,9 @@ ls
 env | grep CMAKE_PREFIX_PATH		
 echo $CMAKE_PREFIX_PATH
 catkin config --extend /opt/ros/indigo
+catkin config --whitelist pluginlib_arrayutil
 catkin build
+catkin config --no-whitelist
 source devel/setup.bash
 echo $CMAKE_PREFIX_PATH
 ls		
