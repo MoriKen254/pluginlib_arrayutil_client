042355824d1d191a7b080ca611c4821a4d4b6a3e
MoriKen254 <masamorobo@gmail.com>
2017-06-04 00:54:46 +0900

    Restore travis

---
+++ .before.sh
@@ -0,0 +1,12 @@
+-#!/bin/bash		
+ -		
+ -pwd		
+ -ls		
+ -cd ../../catkin_ws/src		
+ -pwd		
+ -ls		
+ -env | grep CMAKE_PREFIX_PATH		
+ -sudo catkin install		
+ -ls		
+ -cd /root/src/pluginlib_arrayutil_client		
+ -pwd
