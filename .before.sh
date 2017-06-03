fbd0b5d4a71f7f7be0d08b4837773793e5e37b78
MoriKen254 <masamorobo@gmail.com>
2017-06-04 01:20:21 +0900

    check travis

--- .before.sh
+++ .before.sh
@@ -1,12 +1,16 @@
 -#!/bin/bash		
  -		
  -pwd		
- -ls		
- -cd ../../catkin_ws/src		
- -pwd		
- -ls		
- -env | grep CMAKE_PREFIX_PATH		
- -sudo catkin install		
- -ls		
- -cd /root/src/pluginlib_arrayutil_client		
- -pwd
+ls		
+cd ../../catkin_ws/src		
+pwd		
+ls		
+env | grep CMAKE_PREFIX_PATH		
+echo $CMAKE_PREFIX_PATH
+catkin config --extend /opt/ros/indigo
+catkin build
+source devel/setup.bash
+echo $CMAKE_PREFIX_PATH
+ls		
+cd /root/src/pluginlib_arrayutil_client		
+pwd
