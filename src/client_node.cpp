170ae5389c26d6296bd648b5779d526a903b5a15
MoriKen254 <masamorobo@gmail.com>
2017-06-22 10:28:01 +0900

    Modify client node

--- src/client_node.cpp
+++ src/client_node.cpp
@@ -1,11 +1,25 @@
 #include <pluginlib_arrayutil_client/pluginlib_arrayutil_client.h>
+#include <ros/ros.h>
 
-int main()
+int main(int argc, char** argv)
 {
   try
   {
-    pluginlib_arrayutil_client::PluginlibArrayutilClient client;
-    client.run();
+    ros::init(argc, argv, "client_node");
+    ros::NodeHandle n;
+    ros::Rate loop_rate(1);
+    int loop_count = 0;
+
+    while(ros::ok())
+    {
+      ROS_INFO_STREAM("Loop count: " << ++loop_count);
+
+      pluginlib_arrayutil_client::PluginlibArrayutilClient client;
+      client.run();
+
+      ros::spinOnce();
+      loop_rate.sleep();
+    }
   }
   catch(pluginlib::PluginlibException& ex)
   {
